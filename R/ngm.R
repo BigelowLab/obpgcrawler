c1085aa066394e8047c6b06eb13b5a3c040416ff
Ben Tupper <btupper@bigelow.org>
2019-01-17 07:50:10 -0500

    fix for R 3.5

--- R/ngm.R
+++ R/ngm.R
@@ -1,19 +1,7 @@
-# The Nested Grid Mode, Norman A. Phillips, April 1979
-# NOAA Technical Report NWS 22
-
-# Outer Grid A
-# inner grids B and C
-
-# GRN Greenwich meridian
-# a = radius of earth
-# theta = geographic lat
-# lambda = geographic lon
-
-# 1.5
-# [x,y] <- 2a*(cos(theta)/(1+sin(theta))*[cos(lambda), sin(lambda)]
-
 #' Compute the grid x,y locations from longitude and latitude
-#' 
+#'
+#' From The Nested Grid Mode, Norman A. Phillips, April 1979, NOAA Technical Report NWS 22
+#'
 #' @param theta numeric, geographic lat
 #' @param lambda numeric, geographic lon
 #' @param a numeric, radius of earth
@@ -22,16 +10,10 @@ grid_center <- function(theta = 45, lambda = 45, a = 1){
    2*a*(cos(theta)/(1+sin(theta)))* c(cos(lambda), sin(lambda))
 }
 
-# 1.6
-# The outer grid, denoted by A, is a square array in which i and j each run in the x and y directions from 1 through 2•NH+6. The North Pole on this grid is located at
-#   i =  j =  NH+4.
-
-# 1.7
-# integer NH determines the horizontal grid spacing delta on this grid
-# deltaA = 2a/(NH + 0.5)
-
 #' Compute the grid delta
-#' 
+#'
+#' From The Nested Grid Mode, Norman A. Phillips, April 1979, NOAA Technical Report NWS 22
+#'
 #' @param NH integer, grid spacing
 #' @param a radius of the earth
 #' @return numeric
